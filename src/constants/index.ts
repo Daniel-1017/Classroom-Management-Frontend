250f571407c03a4d462cab563ae55c67e14b27a7
frimudaniel <frimudaniel17@gmail.com>
2026-03-15 15:04:59 +0200

    feat: Implement upload image widget

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -59,8 +59,7 @@ export const ALLOWED_TYPES = [
 const getEnvVar = (key: string): string => {
   const value = import.meta.env[key];
   if (!value) {
-    // throw new Error(`Missing environment variable: ${key}`);
-    return "";
+    throw new Error(`Missing environment variable: ${key}`);
   }
   return value;
 };
